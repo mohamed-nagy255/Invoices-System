723ea0dd55e85a037a7be0c863e039776f18d49f
mohamed-nagy255 <mohamednagy767@gmail.com>
2023-09-16 01:22:54 +0300

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.masterWithoutNav')
+@section('content')
+    <div class="wrapper vh-100">
+        <div class="row align-items-center h-100">
+            <form class="col-lg-3 col-md-4 col-10 mx-auto text-center" method="POST" action="{{ route('login') }}">
+                @csrf
+                {{-- <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.html"> --}}
+                <svg version="1.1" id="logo" class="navbar-brand-img brand-md" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120" xml:space="preserve">
+                    <g>
+                    <polygon class="st0" points="78,105 15,105 24,87 87,87 	" />
+                    <polygon class="st0" points="96,69 33,69 42,51 105,51 	" />
+                    <polygon class="st0" points="78,33 15,33 24,15 87,15 	" />
+                    </g>
+                </svg>
+                {{-- </a> --}}
+                <h1 class="mb-3">Invoices System</h1>
+                <div class="form-group">
+                    @error('inputEmail')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                    <label for="email" class="sr-only">البريد الالكتروني</label>
+                    <input type="email" name="email" id="email" class="form-control form-control-lg @error('email') is-invalid @enderror" placeholder="البريد الالكتروني" required="" autofocus="">
+                </div>
+                <div class="form-group">
+                    @error('password')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                    <label for="password" class="sr-only">كلمة المرور</label>
+                    <input type="password" name="password" id="password" class="form-control form-control-lg @error('password') is-invalid @enderror" placeholder="كلمة المرور" required="">
+                </div>
+                <div class="checkbox mb-3">
+                    <label>
+                    <input type="checkbox" name="remember" id="remember_me"> تذكرني </label>
+                </div>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">تسجيل الدخول</button>
+                <p class="mt-5 mb-3 text-muted">©CONSOLE 2023</p>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="ENG : Mohamed Nagy">
+        <link rel="icon" href="favicon.ico">
+        <title>@yield('title') | برنامج الفواتير</title>
+        
+        @include('layouts.head-css')
+
+    </head>
+    <body class="vertical  light rtl ">
+        <div class="wrapper">
+
+            @include('layouts.nav')
+
+            @include('layouts.sidbar')
+
+            @include('layouts.mainBody')
+
+            @yield('content')
+
+            @include('layouts.modelNati')
+
+        </div> <!-- .wrapper -->
+
+        @include('layouts.footer-js')
+
+    </body>
+</html>
