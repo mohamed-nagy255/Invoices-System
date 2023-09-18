7227a3f2433bba93ab290b56953d88a0ffeaea46
mohamed-nagy255 <mohamednagy767@gmail.com>
2023-09-19 01:44:01 +0300

    commit

--- resources/views/layouts/sidbar.blade.php
+++ resources/views/layouts/sidbar.blade.php
@@ -16,8 +16,8 @@
                     </a>
                 </div>
                 <ul class="navbar-nav flex-fill w-100 mb-2">
-                    <li class="nav-item active">
-                        <a href="/" class="nav-link active">
+                    <li class="nav-item {{ (request()->is('/')) ? 'active' : '' }}">
+                        <a href="/" class="nav-link">
                             <i class="fe fe-home fe-16"></i>
                             <span class="ml-3 item-text">الرئيسية</span>
                         </a>
@@ -27,7 +27,7 @@
                     <span>الفواتير</span>
                 </p>
                 <ul class="navbar-nav flex-fill w-100 mb-2">
-                    <li class="nav-item dropdown">
+                    {{-- <li class="nav-item dropdown">
                     <a href="#ui-elements" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
                         <i class="fe fe-box fe-16"></i>
                         <span class="ml-3 item-text">UI elements</span>
@@ -66,11 +66,11 @@
                         <span class="ml-3 item-text">Widgets</span>
                         <span class="badge badge-pill badge-primary">New</span>
                     </a>
-                    </li>
+                    </li> --}}
                     <li class="nav-item dropdown">
                     <a href="#forms" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
                         <i class="fe fe-credit-card fe-16"></i>
-                        <span class="ml-3 item-text">Forms</span>
+                        <span class="ml-3 item-text"> ادارة الفواتير</span>
                     </a>
                     <ul class="collapse list-unstyled pl-4 w-100" id="forms">
                         <li class="nav-item">
@@ -93,7 +93,7 @@
                         </li>
                     </ul>
                     </li>
-                    <li class="nav-item dropdown">
+                    {{-- <li class="nav-item dropdown">
                     <a href="#tables" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
                         <i class="fe fe-grid fe-16"></i>
                         <span class="ml-3 item-text">Tables</span>
@@ -109,11 +109,11 @@
                         <a class="nav-link pl-3" href="./table_datatables.html"><span class="ml-1 item-text">Data Tables</span></a>
                         </li>
                     </ul>
-                    </li>
+                    </li> --}}
                     <li class="nav-item dropdown">
                     <a href="#charts" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
                         <i class="fe fe-pie-chart fe-16"></i>
-                        <span class="ml-3 item-text">Charts</span>
+                        <span class="ml-3 item-text">التقارير</span>
                     </a>
                     <ul class="collapse list-unstyled pl-4 w-100" id="charts">
                         <li class="nav-item">
@@ -130,150 +130,35 @@
                         </li>
                     </ul>
                     </li>
-                </ul>
-                <p class="text-muted nav-heading mt-4 mb-1">
-                    <span>Apps</span>
-                </p>
-                <ul class="navbar-nav flex-fill w-100 mb-2">
-                    <li class="nav-item w-100">
-                    <a class="nav-link" href="calendar.html">
-                        <i class="fe fe-calendar fe-16"></i>
-                        <span class="ml-3 item-text">Calendar</span>
-                    </a>
+                    <li class="nav-item dropdown">
+                        <a href="#profile" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+                            <i class="fe fe-user fe-16"></i>
+                            <span class="ml-3 item-text">المستخدمين</span>
+                        </a>
+                        <ul class="collapse list-unstyled pl-4 w-100" id="profile">
+                            <a class="nav-link pl-3" href="./profile.html"><span class="ml-1">Overview</span></a>
+                            <a class="nav-link pl-3" href="./profile-settings.html"><span class="ml-1">Settings</span></a>
+                            <a class="nav-link pl-3" href="./profile-security.html"><span class="ml-1">Security</span></a>
+                            <a class="nav-link pl-3" href="./profile-notification.html"><span class="ml-1">Notifications</span></a>
+                        </ul>
                     </li>
                     <li class="nav-item dropdown">
-                    <a href="#contact" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
-                        <i class="fe fe-book fe-16"></i>
-                        <span class="ml-3 item-text">Contacts</span>
-                    </a>
-                    <ul class="collapse list-unstyled pl-4 w-100" id="contact">
-                        <a class="nav-link pl-3" href="./contacts-list.html"><span class="ml-1">Contact List</span></a>
-                        <a class="nav-link pl-3" href="./contacts-grid.html"><span class="ml-1">Contact Grid</span></a>
-                        <a class="nav-link pl-3" href="./contacts-new.html"><span class="ml-1">New Contact</span></a>
-                    </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                    <a href="#profile" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
-                        <i class="fe fe-user fe-16"></i>
-                        <span class="ml-3 item-text">Profile</span>
-                    </a>
-                    <ul class="collapse list-unstyled pl-4 w-100" id="profile">
-                        <a class="nav-link pl-3" href="./profile.html"><span class="ml-1">Overview</span></a>
-                        <a class="nav-link pl-3" href="./profile-settings.html"><span class="ml-1">Settings</span></a>
-                        <a class="nav-link pl-3" href="./profile-security.html"><span class="ml-1">Security</span></a>
-                        <a class="nav-link pl-3" href="./profile-notification.html"><span class="ml-1">Notifications</span></a>
-                    </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                    <a href="#fileman" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
-                        <i class="fe fe-folder fe-16"></i>
-                        <span class="ml-3 item-text">File Manager</span>
-                    </a>
-                    <ul class="collapse list-unstyled pl-4 w-100" id="fileman">
-                        <a class="nav-link pl-3" href="./files-list.html"><span class="ml-1">Files List</span></a>
-                        <a class="nav-link pl-3" href="./files-grid.html"><span class="ml-1">Files Grid</span></a>
-                    </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                    <a href="#support" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
-                        <i class="fe fe-compass fe-16"></i>
-                        <span class="ml-3 item-text">Help Desk</span>
-                    </a>
-                    <ul class="collapse list-unstyled pl-4 w-100" id="support">
-                        <a class="nav-link pl-3" href="./support-center.html"><span class="ml-1">Home</span></a>
-                        <a class="nav-link pl-3" href="./support-tickets.html"><span class="ml-1">Tickets</span></a>
-                        <a class="nav-link pl-3" href="./support-ticket-detail.html"><span class="ml-1">Ticket Detail</span></a>
-                        <a class="nav-link pl-3" href="./support-faqs.html"><span class="ml-1">FAQs</span></a>
-                    </ul>
+                        <a href="#settings" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+                            <i class="fe fe-tool fe-16"></i>
+                            <span class="ml-3 item-text">الاعدادات</span>
+                        </a>
+                        <ul class="collapse list-unstyled pl-4 w-100" id="settings">
+                            <a class="nav-link pl-3" href="./profile.html"><span class="ml-1">Overview</span></a>
+                            <a class="nav-link pl-3" href="./profile-settings.html"><span class="ml-1">Settings</span></a>
+                            <a class="nav-link pl-3" href="./profile-security.html"><span class="ml-1">Security</span></a>
+                            <a class="nav-link pl-3" href="./profile-notification.html"><span class="ml-1">Notifications</span></a>
+                        </ul>
                     </li>
                 </ul>
-                <p class="text-muted nav-heading mt-4 mb-1">
-                    <span>Extra</span>
-                </p>
-                <ul class="navbar-nav flex-fill w-100 mb-2">
-                    <li class="nav-item dropdown">
-                    <a href="#pages" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
-                        <i class="fe fe-file fe-16"></i>
-                        <span class="ml-3 item-text">Pages</span>
-                    </a>
-                    <ul class="collapse list-unstyled pl-4 w-100 w-100" id="pages">
-                        <li class="nav-item">
-                        <a class="nav-link pl-3" href="./page-orders.html">
-                            <span class="ml-1 item-text">Orders</span>
-                        </a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link pl-3" href="./page-timeline.html">
-                            <span class="ml-1 item-text">Timeline</span>
-                        </a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link pl-3" href="./page-invoice.html">
-                            <span class="ml-1 item-text">Invoice</span>
-                        </a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link pl-3" href="./page-404.html">
-                            <span class="ml-1 item-text">Page 404</span>
-                        </a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link pl-3" href="./page-500.html">
-                            <span class="ml-1 item-text">Page 500</span>
-                        </a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link pl-3" href="./page-blank.html">
-                            <span class="ml-1 item-text">Blank</span>
-                        </a>
-                        </li>
-                    </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                    <a href="#auth" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
-                        <i class="fe fe-shield fe-16"></i>
-                        <span class="ml-3 item-text">Authentication</span>
-                    </a>
-                    <ul class="collapse list-unstyled pl-4 w-100" id="auth">
-                        <a class="nav-link pl-3" href="./auth-login.html"><span class="ml-1">Login 1</span></a>
-                        <a class="nav-link pl-3" href="./auth-login-half.html"><span class="ml-1">Login 2</span></a>
-                        <a class="nav-link pl-3" href="./auth-register.html"><span class="ml-1">Register</span></a>
-                        <a class="nav-link pl-3" href="./auth-resetpw.html"><span class="ml-1">Reset Password</span></a>
-                        <a class="nav-link pl-3" href="./auth-confirm.html"><span class="ml-1">Confirm Password</span></a>
-                    </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                    <a href="#layouts" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
-                        <i class="fe fe-layout fe-16"></i>
-                        <span class="ml-3 item-text">Layout</span>
-                    </a>
-                    <ul class="collapse list-unstyled pl-4 w-100" id="layouts">
-                        <li class="nav-item">
-                        <a class="nav-link pl-3" href="./index.html"><span class="ml-1 item-text">Default</span></a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link pl-3" href="./index-horizontal.html"><span class="ml-1 item-text">Top Navigation</span></a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link pl-3" href="./index-boxed.html"><span class="ml-1 item-text">Boxed</span></a>
-                        </li>
-                    </ul>
-                    </li>
-                </ul>
-                <p class="text-muted nav-heading mt-4 mb-1">
-                    <span>Documentation</span>
-                </p>
-                <ul class="navbar-nav flex-fill w-100 mb-2">
-                    <li class="nav-item w-100">
-                    <a class="nav-link" href="../docs/index.html">
-                        <i class="fe fe-help-circle fe-16"></i>
-                        <span class="ml-3 item-text">Getting Start</span>
-                    </a>
-                    </li>
-                </ul>
+                
                 <div class="btn-box w-100 mt-4 mb-1">
-                    <a href="https://themeforest.net/item/tinydash-bootstrap-html-admin-dashboard-template/27511269" target="_blank" class="btn mb-2 btn-primary btn-lg btn-block">
-                    <i class="fe fe-shopping-cart fe-12 mr-2"></i><span class="small">Buy now</span>
+                    <a href="#" target="_blank" class="btn mb-2 btn-primary btn-lg btn-block">
+                    <i class="fe fe-phone-missed fe-12 mr-2"></i><span class="small">تواصل معنا</span>
                     </a>
                 </div>
                 </nav>
