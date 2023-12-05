181f6523fdfd9cf9f231c04115ec5a4cc52813e3
mohamed-nagy255 <mohamednagy767@gmail.com>
2023-12-05 02:16:50 +0200

    commit

--- app/Http/Controllers/InvoiceAttachmentController.php
+++ app/Http/Controllers/InvoiceAttachmentController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class InvoiceAttachmentController extends Controller
 {
-    //
+    protected $table = 'invoice_attachments';
+    protected $fillable = [
+        'file_name',
+        'invoice_id',
+        'invoice_number',
+        'Created_by',
+    ];
 }
